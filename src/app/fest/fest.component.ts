7b0eb06e849ebf0d1af6bdd5b9b1515d9965daf0
Varun_Karthik <varunkarthik246@gmail.com>
2019-12-09 15:32:35 +0530

    github installed

---
+++ src/app/fest/fest.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-fest',
+    templateUrl:'./fest.component.html',
+    styleUrls:['./fest.component.css']
+})
+
+export class FestComponent {}
---
+++ src/app/top/top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+    selector:"app-top",
+    templateUrl:"./top.component.html",
+    styleUrls:["./top.component.css"]
+})
+
+export class TopHeader
+{
+}
